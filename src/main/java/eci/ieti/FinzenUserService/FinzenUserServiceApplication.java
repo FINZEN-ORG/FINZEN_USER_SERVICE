37f439b709f6acce399d5914f59e99f21f6d8794
Samuel Felipe Diaz <samuelfdm14@gmail.com>
2025-09-21 21:30:15 -0500

    Feat: Add initial configuration

---
+++ src/main/java/eci/ieti/FinzenUserService/FinzenUserServiceApplication.java
@@ -0,0 +1,13 @@
+package eci.ieti.FinzenUserService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinzenUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinzenUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eci/ieti/FinzenUserService/FinzenUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package eci.ieti.FinzenUserService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinzenUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
